015575ab08fd792e3b0294c67a8866a5dc2287df
Serega1067 <serega1067@yandex.ru>
2024-06-23 17:25:44 +0700

    Init git applicationWithRecipes

---
+++ README.md
@@ -0,0 +1,8 @@
+# An application with recipes.
+Select entities.  
+Define properties.  
+Endow an entity with actions.
+
+In this application there will be a list of dishes divided into categories.  
+There will be cooking instructions for the recipe. You can add it to your  
+favorites or send it to someone from your contacts.
---
+++ src/main/kotlin/applicationWithRecipes/Dish.kt
@@ -0,0 +1,29 @@
+package main.kotlin.applicationWithRecipes
+
+class Dish(
+    val id: Int,
+    val name: String,
+    var category: String,
+    val ingredients: List<String>,
+    var inFavorites: Boolean = false,
+) {
+    fun addToFavorites() {
+        println("Блюдо $name добавлено в избранное")
+        inFavorites = true
+    }
+
+    fun removeFromFavorites() {
+        println("Блюдо $name удалено из избранного")
+        inFavorites = false
+    }
+
+    fun startCooking() {
+        println(
+            "Пользователь перешёл на экран начала приготовления блюда $name"
+        )
+    }
+
+    fun downloadIngredients(): List<String> {
+        return ingredients
+    }
+}
